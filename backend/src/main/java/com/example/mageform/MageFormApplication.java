f429a53f37cb189f6d71a12e2c11831896f419b0
rex51920594 <andy082220@gmail.com>
2024-02-28 03:19:53 +0800

    串接cloud config

--- backend/src/main/java/com/example/mageform/MageFormApplication.java
+++ backend/src/main/java/com/example/mageform/MageFormApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MageFormApplication extends SpringBootServletInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MageFormApplication.class, args);
--- backend/src/main/java/com/example/mageform/controller/Hello.java
+++ backend/src/main/java/com/example/mageform/controller/Hello.java
@@ -1,14 +1,25 @@
 package com.example.mageform.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
+@RefreshScope
 @RequestMapping("/api")
 public class Hello {
-    	@GetMapping("/employees")
+
+	
+    @Value("${test.text}")
+    private String defaultPageTitle;
+
+	@GetMapping("/employees")
 	public String getAllEmployees(){
+		System.out.println(defaultPageTitle);
 		return "hello";
 	}	
 }
